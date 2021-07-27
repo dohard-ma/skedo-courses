c31f7adffe2c2e94afedc36b0463b438241fa238
ramroll <ramroll@gmail.com>
2021-07-27 22:36:08 +0800

    状态管理

---
+++ skedo-starters/src/day5/example.ts
@@ -0,0 +1,49 @@
+
+import {Action, applyMiddleware, createStore} from 'redux'
+import {combineReducers, skedoReduxMiddleWare} from './skedo-redux'
+
+type Async<T> = Promise<T> | Generator<T> | (() => Promise<T>) | (() => Generator<T>) | T
+type UserAction = {
+} & Action<any>
+type UserState = {
+  logined : string | (() => Async<string>)
+}
+
+function isUserLoggedIn(){
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      resolve("user is loggedin")
+    }, 1000)
+  })
+  
+}
+
+function reducer(state : UserState = {logined: "not login"}, action : UserAction){
+
+  switch(action.type) {
+    case "CHECK_LOGIN_STATUS":
+      return {
+        ...state,
+        logined : isUserLoggedIn
+      }
+    // case "CHECK_USER_ORDERS" : 
+    //   return {
+    //     ...state,
+    //     orders : getUserOrders
+    //   }
+  }
+  return state
+}
+
+const reducers = combineReducers({
+	user : reducer
+})
+const userStore = createStore(reducers, applyMiddleware(skedoReduxMiddleWare))
+
+userStore.subscribe(() => {
+  console.log('state changed', userStore.getState())
+})
+
+userStore.dispatch({
+  type : 'CHECK_LOGIN_STATUS'
+})
